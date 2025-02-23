722a0ee1eca0dae9ffaf6e77f74ee9688b35d81f
kon-amp <kon.ampatzidis@gmail.com>
2025-02-23 20:44:38 +0200

    chore: another way of writing components

--- src/components/CourseGoal.tsx
+++ src/components/CourseGoal.tsx
@@ -1,13 +1,13 @@
-import { type ReactNode } from "react";
+import { PropsWithChildren} from "react";
 
-interface CourseGoalProps {
-    title: string; 
-    children: ReactNode
-}
+// interface CourseGoalProps {
+//     title: string;
+//     children: ReactNode
+// }
 
 // Another approach to get children props
 // import { type PropsWithChildren} from "react";
-// type CourseGoalProps = PropsWithChildren<{title: string}>;
+type CourseGoalProps = PropsWithChildren<{title: string}>;
 
 export default function CourseGoal( {title, children}: CourseGoalProps) {
     return <article>
@@ -18,3 +18,18 @@ export default function CourseGoal( {title, children}: CourseGoalProps) {
         <button>Delete</button>
     </article>
 }
+
+// Another approach instead of using function
+// const CourseGoal = ({title, children} : CourseGoalProps) => {
+// const CourseGoal: FC<CourseGoalProps> = ({title, children}) => {
+//     return (<article>
+//         <div>
+//             <h2>{title}</h2>
+//             {children}
+//         </div>
+//         <button>Delete</button>
+//     </article>
+//     );
+// };
+
+// export default CourseGoal;
